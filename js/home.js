53e3c2e7f53967c04909bb27fd328f542f3ecf02
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-09-27 21:01:24 +0530

    bug fix

--- js/home.js
+++ js/home.js
@@ -2,7 +2,5 @@ import { refreshList } from "./productUtils.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./firebase.js";
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    refreshList(true);
-  }
+  refreshList(true);
 });
